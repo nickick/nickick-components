cc9cfd8a94a9968902da59a0ab22a252b3a99a81
Nick Hong <me@nickhong.xyz>
2025-01-09 15:09:28 -0500

    Remove ui lib, change namespace

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,17 @@
-import { Carousel } from "@repo/slider-picker";
+import { Carousel } from "@nickick/components/slider-picker";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Carousel>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-        </Carousel>
+        <div>
+          <Carousel>
+            <p className="h-24 w-24 bg-red-500">1</p>
+            <div>2</div>
+            <div>3</div>
+          </Carousel>
+        </div>
       </main>
     </div>
   );
